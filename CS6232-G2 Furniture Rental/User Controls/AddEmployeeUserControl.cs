dbd8bf9a641b91316a9ab60f2fc3a8984fc923a6
Kelli Miller <km00163@my.westga.edu>
2022-03-20 17:39:00 -0500

    implemented Add Employee functionality

--- CS6232-G2 Furniture Rental/User Controls/AddEmployeeUserControl.cs
+++ CS6232-G2 Furniture Rental/User Controls/AddEmployeeUserControl.cs
@@ -1,5 +1,8 @@
-﻿using FurnitureRentalDomain;
+﻿using FurnitureRentalBusiness;
+using FurnitureRentalDomain;
+using FurnitureRentalDomain.Helpers;
 using System;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace CS6232_G2_Furniture_Rental.User_Controls
@@ -9,17 +12,16 @@ namespace CS6232_G2_Furniture_Rental.User_Controls
     /// </summary>
     public partial class AddEmployeeUserControl : UserControl
     {
+        private static EmployeeBusiness _business;
+
         /// <summary>
         /// Add Employee user control constructor
         /// </summary>
         public AddEmployeeUserControl()
         {
-            InitializeComponent();
-        }
+            _business = new EmployeeBusiness();
 
-        private void clearButton_Click(object sender, EventArgs e)
-        {
-            this.ClearForm();
+            InitializeComponent();
         }
 
         private void ClearForm()
@@ -57,6 +59,224 @@ namespace CS6232_G2_Furniture_Rental.User_Controls
             
             this.LoadUSStatesListBox();
             this.stateComboBox.SelectedIndex = -1;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            this.ClearForm();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (FieldsValidated())
+                {
+                    Employee newEmployee = new Employee
+                    {
+                        FirstName = this.firstNameTextBox.Text,
+                        LastName = this.lastNameTextBox.Text,
+                        Address1 = this.address1TextBox.Text,
+                        Address2 = (String.IsNullOrEmpty(this.address2TextBox.Text) ? null : this.address2TextBox.Text),
+                        City = this.cityTextBox.Text,
+                        State = this.stateComboBox.Text,
+                        Zipcode = this.zipcodeTextBox.Text,
+                        Birthdate = this.birthdateDateTimePicker.Value,
+                        Phone = this.phoneTextBox.Text,
+                        Sex = GenderHelper.ParseGender(this.sexComboBox.Text),
+                        UserName = this.userNameTextBox.Text,
+                        IsAdmin = this.isAdminCheckBox.Checked,
+                        Password = this.passwordTextBox.Text
+                    };
+
+                    _business.Add(newEmployee);
+                    MessageBox.Show("Employee added!", "Employee added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid input!" + Environment.NewLine + ex.Message,
+                    "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool FieldsValidated()
+        {
+            if (String.IsNullOrWhiteSpace(this.firstNameTextBox.Text))
+            {
+                addErrorProvider.SetError(this.firstNameTextBox, "First name cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.firstNameTextBox, string.Empty);
+            }
+
+            if (String.IsNullOrWhiteSpace(this.lastNameTextBox.Text))
+            {
+                addErrorProvider.SetError(this.lastNameTextBox, "Last name cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.lastNameTextBox, string.Empty);
+            }
+
+            if (String.IsNullOrWhiteSpace(this.address1TextBox.Text))
+            {
+                addErrorProvider.SetError(this.address1TextBox, "Address cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.address1TextBox, string.Empty);
+            }
+
+            if (String.IsNullOrWhiteSpace(this.cityTextBox.Text))
+            {
+                addErrorProvider.SetError(this.cityTextBox, "City cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.cityTextBox, string.Empty);
+            }
+
+            if (this.stateComboBox.SelectedIndex < 0)
+            {
+                addErrorProvider.SetError(this.stateComboBox, "State cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.stateComboBox, string.Empty);
+            }
+
+            if (!this.ValidateZipcode())
+            {
+                addErrorProvider.SetError(this.zipcodeTextBox, "Invalid zipcode");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.zipcodeTextBox, string.Empty);
+            }
+
+            if (this.birthdateDateTimePicker.Value >= DateTime.Today)
+            {
+                addErrorProvider.SetError(this.birthdateDateTimePicker, "Invalid birthdate");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.birthdateDateTimePicker, string.Empty);
+            }
+
+            if (!this.ValidatePhone())
+            {
+                addErrorProvider.SetError(this.phoneTextBox, "Invalid phone number");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.phoneTextBox, string.Empty);
+            }
+
+            if (this.sexComboBox.SelectedIndex < 0)
+            {
+                addErrorProvider.SetError(this.sexComboBox, "Sex cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.sexComboBox, string.Empty);
+            }
+
+            if (String.IsNullOrWhiteSpace(this.userNameTextBox.Text))
+            {
+                addErrorProvider.SetError(this.userNameTextBox, "Username cannot be blank");
+                return false;
+            }
+            else
+            {
+                addErrorProvider.SetError(this.userNameTextBox, string.Empty);
+            }
+
+            if (String.IsNullOrEmpty(this.passwordTextBox.Text))
+            {
+                addErrorProvider.SetError(this.passwordTextBox, "Password cannot be blank");
+            }
+            else
+            {
+                addErrorProvider.SetError(this.passwordTextBox, string.Empty);
+            }
+
+            if (this.passwordTextBox.Text != this.reenterPasswordTextBox.Text)
+            {
+                addErrorProvider.SetError(this.passwordTextBox, "Passwords do not match");
+            }
+            else
+            {
+                addErrorProvider.SetError(this.passwordTextBox, string.Empty);
+            }
+
+            if (!validatePassword())
+            {
+                addErrorProvider.SetError(this.passwordTextBox, "Password does not meet complexity requirements");
+            }
+            else
+            {
+                addErrorProvider.SetError(this.passwordTextBox, string.Empty);
+            }
+
+            return true;
+        }
+
+        private bool ValidateZipcode()
+        {
+            if (String.IsNullOrWhiteSpace(this.zipcodeTextBox.Text))
+            {
+                return false;
+            }
+
+            var _usZipRegEx = @"^\d{5}(?:[-\s]\d{4})?$";
+
+            if ((!Regex.Match(this.zipcodeTextBox.Text, _usZipRegEx).Success))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidatePhone()
+        {
+            if (string.IsNullOrEmpty(this.phoneTextBox.Text))
+            {
+                return false;
+            }
+
+            var _usPhoneRegEx = @"\(?\d{3}\)?[-\.]? *\d{3}[-\.]? *[-\.]?\d{4}";
+
+            if ((!Regex.Match(this.phoneTextBox.Text, _usPhoneRegEx).Success))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool validatePassword()
+        {
+            var _passwordRegEx = @"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
+
+            if ((!Regex.Match(this.passwordTextBox.Text, _passwordRegEx).Success))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ FurnitureRentalBusiness/EmployeeBusiness.cs
@@ -0,0 +1,31 @@
+﻿using FurnitureRentalData;
+using FurnitureRentalDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureRentalBusiness
+{
+    public class EmployeeBusiness
+    {
+        private readonly EmployeeDal _dal;
+
+        public EmployeeBusiness()
+        {
+            _dal = new EmployeeDal();
+        }
+
+        public int Add(Employee newEmployee)
+        {
+            if (newEmployee is null)
+            {
+                throw new ArgumentNullException("Employee cannot be null");
+            }
+
+            return this._dal.AddEmployee(newEmployee);
+        }
+    }
+
+}
