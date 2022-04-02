e5c4d26a68e7cf1e9fd0e703c278fdcf32b6f846
Kelli Miller <km00163@my.westga.edu>
2022-04-02 14:52:54 -0500

    created Furniture class and initial search form

---
+++ CS6232-G2 Furniture Rental/View/FurnitureSearchForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace CS6232_G2_Furniture_Rental.View
+{
+    partial class FurnitureSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FurnitureSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FurnitureSearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Furniture Search";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ FurnitureRentalDomain/Furniture.cs
@@ -0,0 +1,38 @@
+﻿
+namespace FurnitureRentalDomain
+{
+    /// <summary>
+    /// A Furniture item as represented in the db
+    /// </summary>
+    class Furniture
+    {
+        /// <summary>
+        /// The furniture's id
+        /// </summary>
+        public int FurnitureID { get; set; }
+        /// <summary>
+        /// The furniture's description
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// The furniture's daily rental rate
+        /// </summary>
+        public decimal DailyRentalRate { get; set; }
+        /// <summary>
+        /// The furniture's quantity owned
+        /// </summary>
+        public int QuantityOwned { get; set; }
+        /// <summary>
+        /// The furniture's name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The furniture's category description
+        /// </summary>
+        public string CategoryDescription { get; set; }
+        /// <summary>
+        /// The furniture's style description
+        /// </summary>
+        public string StyleDescription { get; set; }
+    }
+}
